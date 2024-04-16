c222ec0607e24d2cd01df3ab096a9cedc950d246
hongbeubeu <hongbeubeu@gmail.com>
2024-04-16 23:39:50 +0700

    Spot light

--- LearnOpenGL/LearnOpenGL/window.cpp
+++ LearnOpenGL/LearnOpenGL/window.cpp
@@ -201,11 +201,14 @@ int main()
         
 		// be sure to activate shader when setting uniforms/drawing objects
 		lightingShader.use();
-		lightingShader.setVec3("light.position", lightPos);
+		lightingShader.setVec3("light.position", camera.Position);
+        lightingShader.setVec3("light.direction", camera.Front);
+        lightingShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
+        lightingShader.setFloat("light.outerCutOff", glm::cos(glm::radians(17.5f)));
 		lightingShader.setVec3("viewPos", camera.Position);
         
-		lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
-		lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
+		lightingShader.setVec3("light.ambient", 0.1f, 0.1f, 0.1f);
+		lightingShader.setVec3("light.diffuse", 0.8f, 0.8f, 0.8f);
 		lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
         lightingShader.setFloat("light.constant", 1.0f);
         lightingShader.setFloat("light.linear", 0.09f);
@@ -246,16 +249,16 @@ int main()
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
         // also draw the lamp object
-		lightCubeShader.use();
-		lightCubeShader.setMat4("projection", projection);
-		lightCubeShader.setMat4("view", view);
-		model = glm::mat4(1.0f);
-		model = glm::translate(model, lightPos);
-		model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
-		lightCubeShader.setMat4("model", model);
-
-		glBindVertexArray(lightCubeVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+//		lightCubeShader.use();
+//		lightCubeShader.setMat4("projection", projection);
+//		lightCubeShader.setMat4("view", view);
+//		model = glm::mat4(1.0f);
+//		model = glm::translate(model, lightPos);
+//		model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
+//		lightCubeShader.setMat4("model", model);
+//
+//		glBindVertexArray(lightCubeVAO);
+//        glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
